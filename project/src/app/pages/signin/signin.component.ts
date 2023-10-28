effc9d8307a300a78f97209d3185901e7e10955b
IT20168704 <it20168704@my.sliit.lk>
2023-10-28 20:14:39 +0530

    Integrate the login page with the API

--- project/src/app/pages/signin/signin.component.ts
+++ project/src/app/pages/signin/signin.component.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthenticationService } from './services/authentication.service';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit {
+
+  form!: FormGroup;
+  isLoggingIn = false;
+  isRecoveringPassword = false;
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  login() {
+    this.isLoggingIn = true;
+
+    this.authService.signIn({
+      email: this.form.value.email,
+      password: this.form.value.password
+    }).subscribe({
+      next: () => this.router.navigate(['home']),
+      error: error => {
+        this.isLoggingIn = false;
+        this.snackBar.open(error.message, "OK", {
+          duration: 5000
+        })
+      }
+    });
+  }
+
+  register() {
+    this.isLoggingIn = true;
+
+    this.authService.register({
+      email: this.form.value.email,
+      password: this.form.value.password
+    }).subscribe({
+      next: () => this.router.navigate(['home']),
+      error: error => {
+        this.isLoggingIn = false;
+        this.snackBar.open(error.message, "OK", {
+          duration: 5000
+        })
+      }
+    });
+  }
+
+  recoverPassword() {
+    this.isRecoveringPassword = true;
+
+    this.authenticationService.recoverPassword(
+      this.form.value.email
+    ).subscribe({
+      next: () => {
+        this.isRecoveringPassword = false;
+        this.snackBar.open("You can recover your password in your email account.", "OK", {
+          duration: 5000
+        });
+      },
+      error: error => {
+        this.isRecoveringPassword = false;
+        this.snackBar.open(error.message, "OK", {
+          duration: 5000
+        });
+      }
+    })
+  }
 
 }
--- project/src/app/pages/signin/signin.module.ts
+++ project/src/app/pages/signin/signin.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SigninComponent } from './signin.component';
 import { RouterModule, Routes } from '@angular/router';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 const routes: Routes = [
   {
     path: '',
-    component: SigninComponent
-  },
-  {
-    path: 'other',
     component: SigninComponent
   }
 ]
@@ -23,9 +22,12 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    ReactiveFormsModule,
 
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule
   ]
 })
 export class SigninModule { }
